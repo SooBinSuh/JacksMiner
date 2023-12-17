8d5f4dee1ac4fa1af1d1aae8d87a354b37a7db92
SooBinSuh <mysticraider2@gmail.com>
2023-12-18 02:20:27 +0900

    아이템 뒤집기 추가

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 /** @format */
 
 import { StatusBar } from "expo-status-bar";
-import { Children, PropsWithChildren } from "react";
+import { Children, PropsWithChildren, useState } from "react";
 import {
   Button,
   Dimensions,
@@ -9,6 +9,7 @@ import {
   StyleProp,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
   ViewStyle,
 } from "react-native";
@@ -24,19 +25,67 @@ export default () => {
     </SafeAreaProvider>
   );
 };
+
+export type Block = {
+  isSelected: boolean;
+};
+const makeArray = (rows: number, columns: number) => {
+  let arr = [];
+  for (let i = 0; i < columns; i++) {
+    arr.push(new Array(rows).fill({ isSelected: false }));
+  }
+  return arr;
+};
+
+function replaceItemAtRowColumn<T>(
+  arr: T[][],
+  row: number,
+  column: number,
+  newValue: T
+) {
+  let newRow = [
+    ...arr[row].slice(0, column),
+    newValue,
+    ...arr[row].slice(column + 1),
+  ];
+  let newArray = [...arr.slice(0, row), newRow, ...arr.slice(row + 1)];
+  return newArray;
+}
+
 function App() {
+  // MARKER: 게임 설정 및 블럭 크기 계산하는 부분
   const insets = useSafeAreaInsets();
-  const widthBlock = 14;
-  const heightBlock = 32;
+
+  const widthBlock = 6; //가로 블록 계수
+  const heightBlock = 6; //세로 블록 계수
+  const mineCount = 4; //지뢰 계수
+
   const windowWidth = Dimensions.get("window").width;
   const windowHeight =
     Dimensions.get("window").height - insets.top - insets.bottom - 50;
-  const rows = [...Array(heightBlock).keys()];
-  const columns = [...Array(widthBlock).keys()];
+  const [blocks, setBlocks] = useState(makeArray(widthBlock, heightBlock));
+
   const itemWidth = Math.min(
     windowWidth / widthBlock,
     windowHeight / heightBlock
   );
+
+  const onItemPress = (index: number[], item: Block) => {
+    if (item.isSelected) {
+      console.log("중복 클릭 불가!");
+      return;
+    }
+
+    const row = index[0];
+    const column = index[1];
+
+    setBlocks(
+      replaceItemAtRowColumn<Block>(blocks, row, column, {
+        ...item,
+        isSelected: true,
+      })
+    );
+  };
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
@@ -46,7 +95,7 @@ function App() {
           <Button
             title="Reset Button"
             onPress={() => {
-              console.log("will reset");
+              // console.log("will reset");
             }}
           />
           <Text>time passed area</Text>
@@ -59,20 +108,50 @@ function App() {
             alignItems: "center",
           }}
         >
-          {rows.map((item, rowIndex) => (
+          {blocks.map((row, rowIndex) => (
+            <View key={rowIndex} style={{ flexDirection: "row" }}>
+              {row.map((item, colIndex) => (
+                <TouchableOpacity
+                  disabled={item.isSelected}
+                  key={[rowIndex, colIndex].toString()}
+                  onPress={() => {
+                    onItemPress([rowIndex, colIndex], item);
+                  }}
+                  style={{
+                    minWidth: itemWidth,
+                    maxWidth: itemWidth,
+                    height: itemWidth,
+                    backgroundColor: item.isSelected
+                      ? "green"
+                      : "rgba(249, 180, 45, 0.25)",
+                  }}
+                >
+                  <Item />
+                </TouchableOpacity>
+              ))}
+            </View>
+          ))}
+          {/* {rows.map((item, rowIndex) => (
             <View key={rowIndex} style={{ flexDirection: "row" }}>
               {columns.map((column, colIndex) => (
-                <Item
+                <TouchableOpacity
                   key={[rowIndex, colIndex].toString()}
+                  onPress={() => {
+                    onItemPress([rowIndex,colIndex]);
+                  }}
                   style={{
                     minWidth: itemWidth,
                     maxWidth: itemWidth,
                     height: itemWidth,
                   }}
-                />
+                >
+                  <Item
+                   
+                  />
+                </TouchableOpacity>
               ))}
             </View>
-          ))}
+          ))} */}
         </View>
       </View>
     </SafeAreaView>
@@ -118,7 +197,7 @@ const styles = StyleSheet.create({
 
     // my visual styles; not important for grid
     padding: 0,
-    backgroundColor: "rgba(249, 180, 45, 0.25)",
+    // backgroundColor: "rgba(249, 180, 45, 0.25)",
     borderWidth: 1,
     borderColor: "#fff",
   },
